2c3eb50040f71220de77b4ee2307cee0e917e886
bradyDarren <dcgbfinancial@gmail.com>
2025-02-27 12:43:23 -0600

    Additional Commit

--- main.py
+++ main.py
@@ -6,7 +6,7 @@ window.minsize(width=500, height=300)
 
 #Label
 my_label = tkinter.Label(text="I am a Label", font=('Arial', 24, 'bold'))
-my_label.pack()
+my_label.grid(column=0, row=0)
 
 # my_label['text'] = 'Cool New Text'
 # my_label.config(text="Cool New Text")
@@ -14,15 +14,23 @@ my_label.pack()
 # Button
 
 def button_click(): 
-    my_label.config(text="Button Got Clicked")
+    new_text = input.get()
+    my_label.config(text=new_text)
 
 button =tkinter.Button(text="Click Me", command=button_click)
-button.pack()
+button.grid(column=1, row=1)
+
+def button_click(): 
+    new_text = input.get()
+    my_label.config(text=new_text)
+
+button =tkinter.Button(text="Click Me", command=button_click)
+button.grid(column=2, row=0)
+
 
 #Entry 
 
-input = tkinter.Entry()
-input.pack()
-
+input = tkinter.Entry(width=10)
+input.grid(column = 3, row = 2)
 
 window.mainloop()    
