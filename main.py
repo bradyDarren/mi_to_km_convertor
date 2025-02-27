2c626ac10a26940e050fb1d0205b3ec06ab33d78
bradyDarren <dcgbfinancial@gmail.com>
2025-02-26 23:54:34 -0600

    additional commit

--- main.py
+++ main.py
@@ -7,4 +7,22 @@ window.minsize(width=500, height=300)
 #Label
 my_label = tkinter.Label(text="I am a Label", font=('Arial', 24, 'bold'))
 my_label.pack()
+
+# my_label['text'] = 'Cool New Text'
+# my_label.config(text="Cool New Text")
+
+# Button
+
+def button_click(): 
+    my_label.config(text="Button Got Clicked")
+
+button =tkinter.Button(text="Click Me", command=button_click)
+button.pack()
+
+#Entry 
+
+input = tkinter.Entry()
+input.pack()
+
+
 window.mainloop()    
